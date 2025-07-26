0f3ee2d2c5f9005d1cc5fe7712815f76360d964c
lamtnguyen989 <lamtnguyen989@gmail.com>
2025-07-26 06:07:09 -0500

    fix shell code verbose flag and lowering deal.ii version

--- Black-Scholes/Finite-elements/build/run.sh
+++ Black-Scholes/Finite-elements/build/run.sh
@@ -3,10 +3,10 @@
 CORES=$(nproc)  # This is the number of cores the computer has, basically this will pararellize the compilation across all cores
 
 # Determines whether or not you want verbose compiler output (flags, debug info, etc.)
-if ["$1" == "1"]; then
+if [$1 == "1"]; then
     VERBOSE_FLAG="VERBOSE=1"
 else
-    VERBOSE_FLAG="VERBOSE=0"
+    VERBOSE_FLAG=""
 fi
 
 # Clean 
@@ -16,6 +16,6 @@ rm CMakeCache.txt
 rm Makefile
 
 # Main compilation and run command
-cmake ..
+cmake .
 make -j$CORES $VERBOSE_FLAG
 ./black-scholes
