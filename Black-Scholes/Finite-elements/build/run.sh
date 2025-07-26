a01561ff5027c541e1a0a6e03eedd0ea95e0454e
lamtnguyen989 <lamtnguyen989@gmail.com>
2025-07-25 23:49:32 -0500

    added script for automating compilation and run the solver

---
+++ Black-Scholes/Finite-elements/build/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CORES=$(nproc)  # This is the number of cores the computer has, basically this will pararellize the compilation across all cores
+
+# Determines whether or not you want verbose compiler output (flags, debug info, etc.)
+if ["$1" = "1"]; then
+    VERBOSE_FLAG="VERBOSE=1"
+else
+    VERBOSE_FLAG="VERBOSE=0"
+fi
+
+# Main compilation and run command
+cmake ..
+make -j$CORES $VERBOSE_FLAG
+./black-scholes
