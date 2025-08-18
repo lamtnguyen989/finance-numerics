5cad9a5eb570d81c74ab326edde522e487d89711
lamtnguyen989 <lamtnguyen989@gmail.com>
2025-08-18 05:10:55 -0500

    a few touchups

---
+++ Heston/PINN/src/model.py
@@ -0,0 +1,24 @@
+class HestonPINN:
+    def __init__(self):
+        pass
+
+    def residual():
+        pass
+
+    def loss():
+        pass
+
+    def train():
+        pass
+
+    def predict():
+        pass
+
+    def analytical_solution():
+        pass
+
+    def load_model():
+        pass
+
+    def benchmark():
+        pass
