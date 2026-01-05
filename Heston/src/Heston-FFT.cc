0f2d2b2e2c4c9368fd9f1e18dd234e74b9e3d6db
Lam Nguyen <lamtnguyen989@gmail.com>
2026-01-05 07:02:20 -0600

    still scaffolding

--- Heston/src/Heston-FFT.cc
+++ Heston/src/Heston-FFT.cc
@@ -3,13 +3,25 @@
 #include <KokkosFFT.hpp>
 
 using Complex = Kokkos::complex<float>;
+using exec_space = Kokkos::DefaultExecutionSpace;
 
+/* Heston model parameters */
 struct HestonParameters
 {
- 
+    float v0;
+    float kappa;
+    float theta;
+    float rho;
+    float sigma;
 }
 
+
+/* main() */
 int main(int argc, char* argv[])
 {
+    Kokkos::initalize(argc, argv);
+    {
 
+    }
+    Kokkos::finalize();
 }
