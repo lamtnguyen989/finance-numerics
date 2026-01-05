70e7aa4262f3114af0470991446b61aba64d957a
Lam Nguyen <lamtnguyen989@gmail.com>
2026-01-05 06:40:54 -0600

    start with Carr-Madan Heston implementation

---
+++ Heston/src/Heston-FFT.cc
@@ -0,0 +1,15 @@
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Complex.hpp>
+#include <KokkosFFT.hpp>
+
+using Complex = Kokkos::complex<float>;
+
+struct HestonParameters
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+
+}
